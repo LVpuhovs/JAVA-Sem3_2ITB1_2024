f2bd5dbfe05eca16bf8a9961b2f9546452eaa885
localuser <markunauts@gmail.com>
2024-03-14 12:02:02 +0200

    New: Page

--- src/model/Page.java
+++ src/model/Page.java
@@ -1,5 +1,45 @@
 package model;
 
+import java.util.ArrayList;
+
+import model.user.User;
+
 public class Page {
+	private String title;
+	private String description;
+	private ArrayList<User> followers = new ArrayList<User>();
+	private ArrayList<Post> postsInPage = new ArrayList<Post>();
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		if (title != null && title.length() > 5 && title.length() < 50)
+			this.title = title;
+		else
+			this.title = "----------";
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		if (description != null && description.length() > 5 && description.length() < 500)
+			this.description = description;
+	}
+	public ArrayList<User> getFollowers() {
+		return followers;
+	}
+	public ArrayList<Post> getPostsInPage() {
+		return postsInPage;
+	}
+	
+	public Page() {
+		setTitle("Autoserviss Ventspili");
+		setDescription("Autoserviss Ventspili - jaunakaas aktualitates");
+	}
+	
+	public String toString() {
+		return title + "(" + description + ")" + followers.size() + " folowers";
+	}
 	
 }
