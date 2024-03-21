e79ff506dc22de4b540a149c21daeff361944672
localuser <markunauts@gmail.com>
2024-03-21 09:14:04 +0200

    Update: MD5 for password

--- src/model/user/User.java
+++ src/model/user/User.java
@@ -21,8 +21,16 @@ public abstract class User extends GuestUser implements IPostService{
 	}
 	public void setPassword(String password) {
 		//TODO ievietot enkodesanu
-		if (password != null && password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"))
-			this.password = password;
+		if (password != null && password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,40}$")) {
+			try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(password.getBytes());
+			this.password = new String(md.digest());
+			}
+			catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
 		else
 			this.password = "-------------";
 	}
